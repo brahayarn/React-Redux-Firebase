6e18f21630ace84365975bbe717a708d1216efff
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2024-07-21 00:49:37 +0300

    add useEffect

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -2,6 +2,8 @@ import { Grid } from 'semantic-ui-react';
 import EventList from './EventList';
 import EventForm from '../form/EventForm';
 import { sampleData } from '../../../app/api/Data';
+import { useEffect, useState } from 'react';
+import { AppEvent } from '../../../app/types/event';
 
 
 type Props = {
@@ -9,11 +11,18 @@ type Props = {
     setFormOpen: (value: boolean) => void;
 }
 
+
 export default function EventDashboard({formOpen, setFormOpen}: Props) {
+  const [events, setEvents] = useState<AppEvent[]>([]);
+
+  useEffect(() => {
+    setEvents(sampleData);
+  }, []);
+
   return (
     <Grid>
         <Grid.Column width={10}>
-            <EventList events = {sampleData}/>
+            <EventList events = {events}/>
         </Grid.Column>
         <Grid.Column width={6}>
             {formOpen && <EventForm setFormOpen={setFormOpen}/>}
