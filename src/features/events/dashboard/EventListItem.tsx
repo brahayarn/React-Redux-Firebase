a13da019bc7f89339d60fe6b118e0507aa85e823
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2024-07-20 19:42:54 +0300

    add props

--- src/features/events/dashboard/EventListItem.tsx
+++ src/features/events/dashboard/EventListItem.tsx
@@ -1,17 +1,19 @@
 import { Item, SegmentGroup, Segment, ItemGroup, Icon, List, Button } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem() {
+export default function EventListItem({event}: any) {
   return (
     <SegmentGroup>
 
       <Segment>
         <ItemGroup>
           <Item>
-            <Item.Image size="tiny" circular src='/user.png'/>
+            <Item.Image size="tiny" circular src={event.hostPhotoURL}/>
             <Item.Content>
-              <Item.Header content="Event Title"/>
-              <Item.Description content="Hosted by me"/>
+              <Item.Header> {event.title}</Item.Header>
+              <Item.Description>
+                Hosted by {event.hostedBy}
+              </Item.Description>
             </Item.Content>
           </Item>
         </ItemGroup>
@@ -19,22 +21,22 @@ export default function EventListItem() {
 
       <Segment>
         <span>
-          <Icon name="clock"/> Date
-          <Icon name="marker"/> Venue
+          <Icon name="clock"/> {event.date}
+          <Icon name="marker"/> {event.venue}
         </span>
       </Segment>
 
       <Segment secondary>
         <List horizontal>
-          <EventListAttendee/>
-          <EventListAttendee/>
-          <EventListAttendee/>
+          {event.attendees.map((attendee: any) => (
+            <EventListAttendee  key = {attendee.id}attendee={attendee}/>
+          ))}
         </List>
       </Segment>
 
       <Segment clearing>
         <span>
-          Description of the event
+          {event.description}
         </span>
         <Button color="teal" floated="right" content="View"/>
       </Segment>
