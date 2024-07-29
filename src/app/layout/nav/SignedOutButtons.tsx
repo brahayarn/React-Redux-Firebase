bef1d7c8eac9141affaef375a55ccc1235950bac
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2024-07-29 13:18:56 +0300

    add login form

--- src/app/layout/nav/SignedOutButtons.tsx
+++ src/app/layout/nav/SignedOutButtons.tsx
@@ -1,13 +1,13 @@
 import { Button, MenuItem } from 'semantic-ui-react';
+import { useAppDispatch } from '../../store/store';
+import { openModal } from '../../common/modals/modalSlice';
 
-type Props = {
-  setAuth: (auth: boolean) => void;
-}
-
-export default function SignedOutButtons({setAuth}: Props) {
+export default function SignedOutButtons() {
+  const dispatch = useAppDispatch();
   return (
     <MenuItem position='right'>
-              <Button basic inverted content='Login' onClick={() => setAuth(true)}/>
+              <Button basic inverted content='Login' 
+              onClick={() => dispatch(openModal({type: 'LoginForm'}))}/>
               <Button basic inverted content='Register' style={{marginLeft: '0.5em'}} />
     </MenuItem>
   )
---
+++ src/features/auth/LoginForm.tsx
@@ -0,0 +1,51 @@
+import { Form, Button } from "semantic-ui-react";
+import ModalsWrapper from "../../app/common/modals/ModalsWrapper";
+import { FieldValues, useForm } from "react-hook-form";
+import { useAppDispatch } from "../../app/store/store";
+import { closeModal } from "../../app/common/modals/modalSlice";
+
+
+
+export default function LoginForm() {
+    const {register, handleSubmit, formState: {isSubmitting, isValid, isDirty, errors}} = useForm({
+        mode: 'onTouched'
+    });
+    function onSubmit(data: FieldValues) {
+        console.log(data);
+        dispatch(closeModal());
+        
+    }
+    const dispatch = useAppDispatch();
+
+  return (
+    <ModalsWrapper header='Sign into'>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Input
+            defaultValue=''
+            placeholder='Email'
+            {...register('email', {required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/})}
+            error={errors.email?.type  === 'required' && 'Email is required'
+            || 
+            errors.email?.type === 'pattern' && 'Email is not valid'
+            } 
+            />
+            <Form.Input
+            type="password"
+            defaultValue=''
+            placeholder='Password'
+            {...register('password', {required: true})}
+            error={errors.password && 'Password is required'}
+            />
+            <Button 
+            loading={isSubmitting}
+            disabled={!isValid || !isDirty || isSubmitting}
+            type='submit'
+            fluid
+            size='large'
+            color='teal'
+            content='Login'
+            />
+        </Form>
+    </ModalsWrapper>
+  )
+}
